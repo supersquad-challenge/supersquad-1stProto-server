493c2a8cc89abb53e3a155c31f351ad08be2bfa3
sjlee80 <sjlee80@gmail.com>
2023-10-02 00:53:24 +0900

    Fix:cors

--- server/app.js
+++ server/app.js
@@ -50,6 +50,7 @@ app.use(
       'http://localhost:3000',
       'http://52.65.222.108',
       'https://super-squad-liart.vercel.app',
+      'https://superssquad.asia',
     ],
     credentials: true,
   })
