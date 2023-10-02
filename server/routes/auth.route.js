2ec6a91b47d0c23c6e6ef556df4f26c0c2580bfa
sjlee80 <sjlee80@gmail.com>
2023-10-02 17:12:50 +0900

    Fix:cors

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -13,7 +13,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', {
     successReturnToOrRedirect:
-      'https://supersquad-proto-front-kzewzj80v-chrislees-projects.vercel.app/home',
+      'https://supersquad-proto-front.vercel.app/home/onApplication',
     failureRedirect: '/',
     session: true,
   })
