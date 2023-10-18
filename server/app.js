7e84b9f25b51ab7bbbc0d83eed6c57dbceff226d
sjlee80 <sjlee80@gmail.com>
2023-10-18 15:23:23 +0900

    Fix: cross domain

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 1 month
     domain: 'supersquad-proto-front.vercel.app',
     sameSite: 'none',
-    secure: true,
+    //secure: true,
   })
 );
 
