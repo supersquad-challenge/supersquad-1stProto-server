e18a2a829779adbd9a36341c438f11fb5c624e1f
sjlee80 <sjlee80@gmail.com>
2023-10-18 22:08:08 +0900

    fix: cookie

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 
 app.use(
   cookieSession({
-    name: 'cookie-session',
+    name: 'cookie-session2',
     keys: [process.env.SESSION_SECRET],
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 1 month
     //domain: 'supersquad-proto-front.vercel.app',
