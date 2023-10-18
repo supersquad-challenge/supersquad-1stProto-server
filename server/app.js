a4ad65eaa04680b8e7eebd03908ac81add0c586d
sjlee80 <sjlee80@gmail.com>
2023-10-18 23:19:27 +0900

    fix: cookie

--- server/app.js
+++ server/app.js
@@ -18,8 +18,8 @@ app.use(
     name: 'cookie-session5',
     keys: [process.env.SESSION_SECRET],
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 1 month
-    domain: 'supersquad-proto-front.vercel.app',
-    sameSite: 'lax',
+    domain: 'supersquad.site',
+    // sameSite: 'lax',
     // secure: true,
   })
 );
