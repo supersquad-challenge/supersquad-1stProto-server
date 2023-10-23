c5ea16b514a1c04a7719a2a1ac553e412256266f
sjlee80 <sjlee80@gmail.com>
2023-10-23 21:48:33 +0900

    Fix: log

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -13,7 +13,7 @@ router.get(
   '/google/callback',
   passport.authenticate('google', {
     //successReturnToOrRedirect: 'https://proto.supersquad.site',
-    successReturnToOrRedirect: 'https://supersquad.site/challenge',
+    successReturnToOrRedirect: 'https://proto.supersquad.site/challenge',
     // successReturnToOrRedirect: 'http://localhost:3000',
     failureRedirect: '/',
     session: true,
